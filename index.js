205a2cb6ab68da0a0338649ac8fc7eb6cdd3b916
Roman <s91630277@gmail.com>
2015-06-23 16:14:51 +0300

    Fixed trailing lost issue

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
 /* eslint no-constant-condition:0 eol-last:0 */
 "use strict";
+
+var openkey = "{";
+var closekey = "}";
+
 module.exports = function(format, data) {
-	var pieces = [];
+    var pieces = [];
 
-	if (typeof data !== "object" || data === null) {
-		data = Array.prototype.slice.call(arguments, 1);
-	}
+    if (typeof data !== "object" || data === null) {
+        data = Array.prototype.slice.call(arguments, 1);
+    }
 
-	var openkey = "{";
-	var closekey = "}";
+    var position = 0;
 
-	var position = 0;
+    while (true) {
+        var start = format.indexOf(openkey, position);
+        var end = format.indexOf(closekey, position);
 
-	while (true) {
-		var start = format.indexOf(openkey, position);
-		var end = format.indexOf(closekey, position);
+        if (start === -1 || end === -1) {
+            // Insert trail piece.
+            var trail = format.substr(position);
+            if (trail !== "") {
+                pieces.push(trail);
+            }
+            break;
+        }
 
-		if (start === -1 || end === -1) {
-			break;
-		}
+        var key = format.substring(start + openkey.length, end);
+        var value = String(data[key]);
 
-		var key = format.substring(start + openkey.length, end);
-		var value = String(data[key]);
+        var before = format.substring(position, start);
+        pieces.push(before);
 
-		var before = format.substring(position, start);
-		pieces.push(before);
+        pieces.push(value);
 
-		pieces.push(value);
+        position = end + closekey.length;
+    }
 
-		position = end + closekey.length;
-	}
-
-	var result = pieces.join("");
-	return result;
+    var result = pieces.join("");
+    return result;
 };
